5c918d2700eb2003108254c202c0de7aecb29dc1
Joao Ramalhinho <joao.ramalhinho.15@ucl.ac.uk>
2025-01-14 18:22:23 +0000

    Added control buttons for video interaction.

--- gui/main_widget.py
+++ gui/main_widget.py
@@ -7,7 +7,8 @@ Main widget class, where all everything is displayed
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFont, QPixmap, QImage
-from PyQt6.QtWidgets import QWidget, QPushButton, QLabel, QVBoxLayout, QFileDialog, QMessageBox, QComboBox
+from PyQt6.QtWidgets import QWidget, QPushButton, QLabel, QVBoxLayout, QFileDialog, QMessageBox, QComboBox, QHBoxLayout, \
+    QLineEdit
 import data_io.video_reader as vidr
 import threading
 
@@ -22,12 +23,41 @@ class MainWidget(QWidget):
         self.setWindowTitle("Cholec80 Video Explorer")
         # self.setFixedSize(1920, 1080)
 
-        # a button for loading video
-        self.load_button = QPushButton("Load Video")
-        self.load_button.clicked.connect(self.on_load_button_click)
         # a label of "patient"
         self.patient_label = QLabel("Patient ID:")
         self.patient_label.setFont(QFont("Arial", 12))
+
+        # A label for current frame
+        self.frame_label = QLabel("Current Frame")
+        self.frame_label.setFont(QFont("Arial", 12))
+
+        # A label for the current frame rate
+        self.rate_label = QLabel("Frame Rate")
+        self.rate_label.setFont(QFont("Arial", 11))
+
+
+        # a button for loading video
+        self.load_button = QPushButton("Load Video")
+        self.load_button.clicked.connect(self.on_load_button_click)
+
+        # a video frame checker, an interactive text
+        self.frame_edit = QLineEdit()
+        self.frame_edit.setFont(QFont("Arial", 11))
+        self.frame_edit.setText("0")
+        self.frame_edit.setMaximumWidth(30)
+        self.frame_edit.setReadOnly(True)
+
+        # A label with the total number of frames
+        self.total_label = QLabel("out of 0")
+        self.total_label.setFont(QFont("Arial", 11))
+
+        # a video frame checker, an interactive text
+        self.rate_edit = QLineEdit()
+        self.rate_edit.setFont(QFont("Arial", 11))
+        self.rate_edit.setText("1")
+        self.rate_edit.setMaximumWidth(30)
+        self.rate_edit.setReadOnly(True)
+
         # a display for video
         self.video_display = QLabel()
         # Buttons for playing and stepping
@@ -44,15 +74,49 @@ class MainWidget(QWidget):
         self.sampling_button = QComboBox()
 
         # Position objects
-        layout = QVBoxLayout()
-        self.setLayout(layout)
+        main_layout = QHBoxLayout()
+        self.setLayout(main_layout)
+        # Set three columns as sublayouts
+        left_column = QVBoxLayout()
+        middle_column = QVBoxLayout()
+        right_column = QVBoxLayout()
+        # Add the three columns
+        main_layout.addLayout(left_column)
+        main_layout.addLayout(middle_column)
+        main_layout.addLayout(right_column)
 
-        layout.addWidget(self.patient_label)
-        layout.addWidget(self.load_button)
-        layout.addWidget(self.video_display)
-        layout.addWidget(self.next_button)
-        layout.addWidget(self.prev_button)
-        layout.addWidget(self.play_button)
+        # Add objects to the left column, first a row
+        patient_row = QHBoxLayout()
+        patient_row.addWidget(self.patient_label)
+
+        left_column.addLayout(patient_row)
+
+        # Add load and frame checker in the next row
+        load_row = QHBoxLayout()
+        frame_box = QHBoxLayout()
+        frame_box.addWidget(self.frame_edit)
+        frame_box.addWidget(self.total_label)
+        load_row.addWidget(self.load_button)
+        load_row.addWidget(self.frame_label)
+        load_row.addLayout(frame_box)
+        load_row.addWidget(self.rate_label)
+        load_row.addWidget(self.rate_edit)
+        left_column.addLayout(load_row)
+
+        # Add the video display
+        left_column.addWidget(self.video_display)
+
+        # Add the play buttons
+        play_buttons_layout = QHBoxLayout()
+        play_buttons_layout.addWidget(self.prev_button)
+        play_buttons_layout.addWidget(self.play_button)
+        play_buttons_layout.addWidget(self.next_button)
+        left_column.addLayout(play_buttons_layout)
+
+
+
+
+
 
         # Directory of data to display
         self.video_path = None
@@ -102,6 +166,10 @@ class MainWidget(QWidget):
         # Make play and next buttons togglable with qt
         self.next_button.setDisabled(False)
         self.play_button.setDisabled(False)
+
+        # Make edits possible
+        self.frame_edit.setReadOnly(False)
+        self.rate_edit.setReadOnly(False)
 
         return 0
 
