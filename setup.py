780b36fca1e83188a8d435af088721875d8afb01
Joao Ramalhinho <joao.ramalhinho.15@ucl.ac.uk>
2025-01-16 18:06:06 +0000

    Added installation setup.py.

---
+++ setup.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+"""
+Setup for surgical_video_explorer
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='surgical_video_explorer',
+    version='1.0',
+    url='https://github.com/jramalhinho/surgical_video_explorer',
+    author='João Ramalhinho',
+    author_email='jdmramalhinho@gmail.com',
+    install_requires=[
+            'opencv-python==4.10.0.84',
+            'PyQt6==6.8.0',
+            'numpy==2.2.1'
+    ],
+    entry_points={
+        'console_scripts':[
+            'surgical_video_explorer=surgical_video_explorer:main',
+        ]
+    }
+)
